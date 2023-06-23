56624396dba24c6c30cc5ebf2084f4e8cf6b6c35
alishapaul01 <paulalisha32@gmail.com>
2023-06-24 00:47:52 +0530

    async await for previous question

--- promise.js
+++ promise.js
@@ -1,6 +1,11 @@
 const posts = [{title: 'Post One', body: 'This is Post One'}]
 const user = [{lastactivitytime: new Date().getTime()}]
+function getPosts(){
 
+        posts.forEach((post) => {
+            console.log(post.title)
+        })
+    }
 function createPost(post){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
@@ -34,11 +39,35 @@ function deletePost(){
 })
 }
 
-try{
+async function display() {
+
+    try {
+
+            await createPost({title : 'Post Two', body : 'This is Post Two'})
+            
+            await updateuserlastactivitytime()
+            console.log('user last activity time:', user.lastactivitytime)
+            await deletePost();
+            getPosts()
+            await deletePost();
+            getPosts()
+
+        
+
+    }catch(err) {
+
+        console.log(err);
+
+    }
+
+}
+display();
+           
+/*try{
 Promise.all([createPost({title:'Post Five', body: 'This is Post Five'}), updateuserlastactivitytime()]).then(deletePost).then(deletePost)
 
 }
 
 catch(err){ 
     console.log(err)
-}
+}*/
